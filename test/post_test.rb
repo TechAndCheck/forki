ebd0917d9f6ab0a45dbfa8358e16b6b6fd1d3282
Asa Royal <oneroyalace@gmail.com>
2021-10-26 09:52:16 -0400

    Post scraping tests working

--- test/post_test.rb
+++ test/post_test.rb
@@ -9,9 +9,80 @@ class PostTest < Minitest::Test
 
   # Note: if this fails, check the account, the number may just have changed
   # We're using Pete Souza because Obama's former photographer isn't likely to be taken down
-  def test_a_single_image_post_returns_properly_when_scraped
-    post = Zorki::Post.lookup(["https://www.facebook.com/humansofnewyork/photos/a.102107073196735/6698806303526746/"]).first
-    assert_equal post.image_file_names.count, 1
+  def test_an_image_post_by_a_page_returns_properly_when_scraped
+    post = Forki::Post.lookup(["https://www.facebook.com/humansofnewyork/photos/a.102107073196735/6698806303526746/"]).first
+
+    refute post.has_video
+    assert_nil post.num_views
+
+    assert post.num_shares > 0
+    assert post.num_comments > 0
+    assert post.reactions.length > 0
+
+    assert_not_nil post.image_file_name
+
+    assert_not_nil post.user.first
   end
 
+  def test_an_image_post_by_a_user_returns_properly_when_scraped
+    post = Forki::Post.lookup(["https://www.facebook.com/photo.php?fbid=3038249389564729&set=a.104631959593168&type=3"]).first
+    refute post.has_video
+    assert_nil post.num_views  # images do not have "views"
+
+    assert post.num_shares > 0
+    assert post.num_comments > 0
+    assert post.reactions.length > 0
+
+    assert_not_nil post.image_file_name
+
+    assert_not_nil post.user.first
+  end
+
+  def test_a_video_post_by_a_user_returns_properly_when_scraped
+    post = Forki::Post.lookup(["https://www.facebook.com/cory.hurlburt/videos/10163562367665117/"]).first
+    assert post.has_video
+    assert post.num_views > 0
+
+    # assert post.num_shares > 0
+    assert post.num_comments > 0
+    assert post.reactions.length > 0
+
+    assert_not_nil post.video_file_name
+    assert_nil post.image_file_name
+    assert_not_nil post.video_preview_image
+
+    assert_not_nil post.user.first
+  end
+
+  def test_a_video_post_by_a_page_retuns_properly_when_scraped
+    post = Forki::Post.lookup(["https://www.facebook.com/redwhitebluenews/videos/258470355199081/"]).first
+    assert post.has_video
+    assert post.num_views > 0
+
+    # assert post.num_shares > 0
+    assert post.num_comments > 0
+    assert post.reactions.length > 0
+
+    assert_not_nil post.video_file_name
+    assert_nil post.image_file_name
+    assert_not_nil post.video_preview_image
+
+    assert_not_nil post.user.first
+  end
+
+  def test_a_video_by_a_page_in_the_watch_tab_returns_properly_when_scraped
+    post = Forki::Post.lookup(["https://www.facebook.com/watch/?v=3743756062349219"]).first
+    assert post.has_video
+    assert post.num_views > 0
+
+    # assert post.num_shares > 0
+    assert post.num_comments > 0
+    assert post.reactions.length > 0
+
+    assert_not_nil post.video_file_name
+    assert_nil post.image_file_name
+    assert_not_nil post.video_preview_image
+
+    assert_not_nil post.user.first
+  end
 end
